c8f7151d046621e8746542727cb91a5727f82ce1
higor-xavier <higorsxavier@gmail.com>
2023-05-07 17:19:18 -0300

    Creating the View functions

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,13 +3,28 @@
 	namespace App\Controllers;
 
 	class IndexController {
+
+		private $view;
+
+		public function __construct(){
+			$this->view = new \stdClass();
+		}
 		
 		public function index() {
-			echo "IndexController index";
+			$this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+			$this->render('index');			
 		}
 
 		public function sobreNos() {
-			echo "IndexController sobreNos";
+			$this->view->dados = array('Sofá', 'Cadeira', 'Cama', 'Notebook', 'PS5');
+			$this->render('sobreNos');
+		}
+
+		public function render($view) {
+			$classAtual = get_class($this);
+			$classAtual = str_replace('App\\Controllers\\', '', $classAtual);
+			$classAtual = strtolower(str_replace('Controller', '', $classAtual));
+			require_once "../App/Views/".$classAtual."/".$view.".phtml";
 		}
 	}
 

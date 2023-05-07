6a71c8162f85cccb918cfc0ad39fec8a2f758ca5
higor-xavier <higorsxavier@gmail.com>
2023-05-07 18:11:59 -0300

    Controllers abstraction

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,13 +2,11 @@
 
 	namespace App\Controllers;
 
-	class IndexController {
+	use MF\Controller\Action;
 
-		private $view;
+	class IndexController extends Action {
 
-		public function __construct(){
-			$this->view = new \stdClass();
-		}
+
 		
 		public function index() {
 			$this->view->dados = array('Sofá', 'Cadeira', 'Cama');
@@ -18,13 +16,6 @@
 		public function sobreNos() {
 			$this->view->dados = array('Sofá', 'Cadeira', 'Cama', 'Notebook', 'PS5');
 			$this->render('sobreNos');
-		}
-
-		public function render($view) {
-			$classAtual = get_class($this);
-			$classAtual = str_replace('App\\Controllers\\', '', $classAtual);
-			$classAtual = strtolower(str_replace('Controller', '', $classAtual));
-			require_once "../App/Views/".$classAtual."/".$view.".phtml";
 		}
 	}
 
